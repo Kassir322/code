502f00401defc94d57f2fc12b8f53e35914e50b6
Дмитрий Сенцов <kassen.ds@gmail.com>
2025-04-08 16:56:39 +0300

    Мини фикс хедера. Теперь каталог подсвечивается нормально

--- client/src/components/ui/NavLink.js
+++ client/src/components/ui/NavLink.js
@@ -6,12 +6,14 @@ import React from 'react'
 
 export default function NavLink({ children, href }) {
 	const pathname = usePathname()
+	// console.log(pathname.split('/')[1])
+	// console.log(href.split('/')[1])
 
 	return (
 		<Link
 			href={href}
 			className={`sm:font-medium sm:text-xl hover:text-hover transition-colors ${
-				pathname == href && 'text-secondary-blue'
+				pathname.split('/')[1] == href.split('/')[1] && 'text-secondary-blue'
 			}`}
 		>
 			{children}
